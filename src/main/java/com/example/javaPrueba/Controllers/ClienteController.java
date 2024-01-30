93ca9afbae14da7cad8907c99d3b6b7c77af1c04
Facundo Brunetti <facundobrunetti99@gmail.com>
2024-01-30 01:41:15 -0300

    7cfb

--- src/main/java/com/example/javaPrueba/Controllers/ClienteController.java
+++ src/main/java/com/example/javaPrueba/Controllers/ClienteController.java
@@ -1,5 +1,6 @@
 package com.example.javaPrueba.Controllers;
 import com.example.javaPrueba.Repository.ClienteRepository;
+import com.example.javaPrueba.Service.ServiceEdad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.javaPrueba.Models.Cliente;
@@ -9,59 +10,19 @@ import java.util.List;
 
 
 @RestController
-public class ClienteController {
+public class ClienteController extends ServiceEdad {
 
     @Autowired
     private ClienteRepository clienteRepo;
 
-    @GetMapping("cliente/client")
+    @GetMapping("cliente/clientes")
     public List<Cliente> getClientes(){
         return  clienteRepo.findAll();
     }
 
-    @GetMapping("cliente/edad")
-    public void clienteEdad(){
-
-
-
-    }
-    @PostMapping("cliente/nuevo")
+    @PostMapping("cliente/alta")
     public String post(@RequestBody Cliente cliente){
-
-        int diaNac=cliente.getFehcanac().getDayOfMonth();
-        Month mes;
-        mes = cliente.getFehcanac().getMonth();
-        int mesNac=mes.getValue();
-        int añoNac=cliente.getFehcanac().getYear();
-
-
-
-        LocalDate fechaActual = LocalDate.now();
-        int añoActual=fechaActual.getYear();
-        Month mesActualMonth=fechaActual.getMonth();
-         int mesActual=mesActualMonth.getValue();
-         int diaActual=fechaActual.getDayOfMonth();
-
-
-       /*12/1/1999 29/1/2024*/
-
-        int edadOp=0;
-        int mesOp=mesActual-mesNac;
-        int diaOp=diaActual-diaNac;
-
-        if(mesOp==0){/*--0 Se ecnuentra en el mes de cumpleaños*/
-            if(diaOp>0){/*--17 Ya fue su cumpeaños*/
-                edadOp=añoActual-añoNac;/*2024-1999=25*/
-
-            }if(diaOp==0 || diaOp<0){/*se le resta 1. ((2024-1999)=25 -- (25-1)=24) si todavia no cumple años*/
-                edadOp=añoActual-añoNac-1;
-            }
-            }if(mesOp<0){
-            edadOp=añoActual-añoNac-1;
-        }if(mesOp>0){
-            edadOp=añoActual-añoNac;
-        }
-        cliente.setEdad(edadOp);/*25--En este caso, añado edadOp(25) al cliente, donde es el resultado de la operacion*/
+        edadPersona(cliente);
         clienteRepo.save(cliente);
         return "Alta realizada";
     }
---
+++ src/main/java/com/example/javaPrueba/Service/ServiceEdad.java
@@ -0,0 +1,52 @@
+package com.example.javaPrueba.Service;
+import com.example.javaPrueba.Models.Cliente;
+import java.time.LocalDate;
+import java.time.Month;
+
+
+
+public  class ServiceEdad extends Cliente  {
+
+    public void edadPersona(Cliente cliente) {
+                int diaNac = cliente.getFehcanac().getDayOfMonth();
+                Month mes;
+                mes = cliente.getFehcanac().getMonth();
+                int mesNac = mes.getValue();
+                int añoNac = cliente.getFehcanac().getYear();
+                LocalDate fechaActual = LocalDate.now();
+                int añoActual = fechaActual.getYear();
+                Month mesActualMonth = fechaActual.getMonth();
+                int mesActual = mesActualMonth.getValue();
+                int diaActual = fechaActual.getDayOfMonth();
+
+                /*12/1/1999 29/1/2024*/
+
+                int edadOp = 0;
+                int mesOp = mesActual - mesNac;
+                int diaOp = diaActual - diaNac;
+
+                if (mesOp == 0) {/*--0 Se ecnuentra en el mes de cumpleaños*/
+                    if (diaOp > 0) {/*--17 Ya fue su cumpeaños*/
+                        edadOp = añoActual - añoNac;/*2024-1999=25*/
+                    }
+                    if (diaOp == 0 || diaOp < 0) {/*se le resta 1. ((2024-1999)=25 -- (25-1)=24) si todavia no cumple años*/
+                        edadOp = añoActual - añoNac - 1;
+                    }
+                }
+                if (mesOp < 0) {
+                    edadOp = añoActual - añoNac - 1;
+                }
+                if (mesOp > 0) {
+                    edadOp = añoActual - añoNac;
+                }
+                cliente.setEdad(edadOp);/*25--En este caso, añado edadOp(25) al cliente, donde es el resultado de la operacion*/
+
+    }
+
+
+
+
+
+
+}
+
